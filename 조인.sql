e9a350151ad86c8a058f0c66468ef92657ce731c
networkSorcerer <gigattfyhig@gmail.com>
2024-10-17 15:16:44 +0900

    조인

--- 조인.sql
+++ 조인.sql
@@ -22,3 +22,47 @@ select empno, ename, job, sal, e.deptno
 from emp e join dept d
 on e.deptno = d.deptno
 order by empno;
+
+--DEPT테이블과 emp 테이블은 1: N관계를 가짐 (부서테이블의 부서번호에는 여러명의 사원이 올라옴)
+-- 조인에서 출력 범위 설정 하기
+SELECT empno, ename, sal, d.deptno, dname, loc
+FROM emp e JOIN dept d 
+ON e.deptno = d.deptno
+WHERE sal >= 3000;
+
+
+-- EMP 테이블 별칭을 E로, DEPT 테이블 별칭은 D로 하여 다음과 같이
+-- 등가 조인을 했을 때 급여가 2500 이하이고,
+-- 사원 번호가 9999 이하인 사원의 정보가 출력되도록 작성
+-- 오라클 조인이나 ANSI조인 아무거나 사용
+
+SELECT * 
+FROM EMP e JOIN DEPT d 
+ON e.DEPTNO = d.DEPTNO 
+WHERE e.sal <= 2500 AND e.empno <= 9999
+ORDER BY e.empno;
+
+
+
+-- 비등가 조인 : 동일한 컬럼이 존재하지 않는 경우 조인할때 사용, 일반적인 방식은 아님
+select * from salgrade; -- 각 급여에 대한 등급 표시
+select ename, sal, grade
+from emp e JOIN salgrade s
+on sal between s.losal and s.hisal; -- 급여와 losal ~ hisal 비등가 조인 
+
+-- 자체 조인(self join) : 자기 자신의 테이블과 조인하는 것을 말함(같은 테이블을 두번 사용)
+select e1.empno, e1.ename, e1.mgr,
+e2.empno as 상관사원번호,
+e2.ename as 매니저이름
+from emp e1 join emp e2
+on e1.mgr =e2.empno;
+
+-- 외부 조인 (outer join) : Left, Right, Full
+select e.ename, e.deptno, d.dname
+from emp e join dept d 
+on e.deptno = d.deptno
+order by e.deptno;
+
+
+
+
