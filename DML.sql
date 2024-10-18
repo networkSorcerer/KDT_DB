1a8ad84b42b34f27976a91d9d952ab22aa115982
networkSorcerer <gigattfyhig@gmail.com>
2024-10-18 15:06:57 +0900

    DML

---
+++ DML.sql
@@ -0,0 +1,19 @@
+-- DML (data manipulation laguage) : insert(입력), update(수정), delete(삭제)
+-- 연습용 테이블 생성하기 
+create table DEPT_TEMP
+as select * from dept;
+
+select *
+from dept_temp;
+
+-- 테이블에 데이터를 추가하는 insert문
+-- insert into 테이블 명 (컬럼명...) values(데이터...)
+insert into dept_temp(deptno, dname, loc) values (50,'DATABASE','SEOUL');
+
+insert into dept_temp values (60,'BACKEND','BUSAN');
+
+insert into dept_temp(deptno) values(70);
+
+insert into dept_temp values (80,'frontend','inchun');
+insert into dept_temp (dname, loc) values ('app','DAEGU');
+insert into dept_temp values (80,'frontend','inchun');
