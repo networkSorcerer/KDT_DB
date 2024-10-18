b04ff9f9010b5241b7fb1af4a561580472517bb7
networkSorcerer <gigattfyhig@gmail.com>
2024-10-18 15:29:43 +0900

    DML

--- DML.sql
+++ DML.sql
@@ -17,3 +17,31 @@ insert into dept_temp(deptno) values(70);
 insert into dept_temp values (80,'frontend','inchun');
 insert into dept_temp (dname, loc) values ('app','DAEGU');
 insert into dept_temp values (80,'frontend','inchun');
+
+delete from dept_temp
+where deptno =70;
+
+insert  into dept_temp values(70, '웹개발','');
+
+create table emp_temp 
+as select * from emp
+where 1 != 1;
+
+drop table emp_temp;
+
+select *from emp_temp;
+
+insert into emp_temp (empno,ename, job,mgr,hiredate,sal,comm,deptno)
+values (9001,'나영석','pd',null,'2020/01/01',9900,1000,50);
+
+insert into emp_temp (empno,ename, job,mgr,hiredate,sal,comm,deptno)
+values (9002,'나영석','mc',null,to_date('2021/01/02','yyyy/mm/dd'),8000,1000,60);
+
+insert into emp_temp (empno,ename, job,mgr,hiredate,sal,comm,deptno)
+values (9002,'서장훈','mc',null,sysdate,9000,1000,70);
+
+rollback;
+
+select * from dept_temp;
+
+insert into dept_temp (deptno, dname, loc) values (80,'frontend','suwon');
